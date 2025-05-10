9dc9e957781711220a62ceae35a58466bbaa9cd7
Michał Kołodziejczyk <michal94mk@o2.pl>
2025-05-10 08:57:24 +0200

    feat: improve UI elements across pages
    - Make service card images perfectly square and full-width
    - Adjust About Us section layout with centered title
    - Fix vertical alignment of text in About Us section

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -1,14 +1,14 @@
 <section id="aboutus" class="aboutus section-padding">
   <div class="container pt-5">
-    <div class="row">
+    <h1 class="text-center mb-5">About Us</h1>
+    <div class="row align-items-center">
       <div class="col-lg-6 col-md-12">
         <div class="about-img">
           <img src="images/aboutus.jpg" alt="..." class="img-fluid">
         </div>
       </div>
-      <div class="col-lg-6 col-md-12 d-flex align-items-center justify-content-center">
+      <div class="col-lg-6 col-md-12">
         <div class="about-text text-center">
-          <h1 class="mb-4">About Us</h1>
           <p class="mb-4">Welcome to Car Repair – your destination for comprehensive car care! We are proud specialists in the field of automotive mechanics, committed to providing unmatched quality service to our customers. Our company is the result of collaboration between a highly qualified team and the use of cutting-edge technologies in the industry. Thanks to this, we can confidently meet even the most sophisticated expectations.
 Experience our commitment to perfection and professionalism by joining our group of satisfied customers. Here we don't just repair cars – we build lasting relationships based on trust and excellent service. Trust our experience and put your car in the hands of experts who treat it as their own.</p>
         </div>
